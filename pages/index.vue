b4c8a40fd1beecbe048bd8adde0ff800541cb1a9
ristinuraulia2 <144652936+ristinuraulia2@users.noreply.github.com>
2025-01-04 14:08:50 +0700

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container-fluid">
+    <div class="row my-5">
+
+      <div class="col-lg-12 mb-5">
+        <div class="crounded-5">
+          <img src="~/assets/img/guru.jpg" alt="gambar" style="width: 80%; height: 50%;" />
+        </div>
+      </div>
+
+      <div class="col-lg-12">
+        <div class="sambutan rounded-5">
+          <div class="sambutan-content">
+            <img src="~/assets/img/kepalasekolah.jpg" alt="Kepala Sekolah" class="kepala-sekolah-img" />
+
+            <p><strong>- Kurniawan, S.Pd, M.Pd.</strong></p>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  height: 400px;
+  box-shadow: 1px 1px 10px #424242;
+  width: 50vw;
+  max-width: 10000px;
+  padding-left: 100px;
+  padding-right: 100px;
+
+}
+
+.kepala-sekolah-img {
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+
+
+@media (max-width: 768px) {
+  .sambutan-content {
+    align-items: center;
+  }
+
+  .card {
+    width: 90vw;
+    padding-left: 50px;
+    padding-right: 50px;
+  }
+}
+</style>
---
+++ pages/jurusan/index.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="container py-5">
+    <div class="row g-4">
+      <div class="col-md-4">
+        <div class="jurusan-card">
+          <img src="assets/img/tkj.jpeg" alt="TJKT" />
+          <h3>Teknik Jaringan Komputer dan Telekomunikasi</h3>
+          <p>Program keahlian yang mempelajari tentang jaringan komputer, sistem telekomunikasi, dan keamanan jaringan.
+          </p>
+        </div>
+      </div>
+
+      <div class="col-md-4">
+        <div class="jurusan-card">
+          <img src="assets/img/rpl.jpeg" alt="RPL" />
+          <h3>Pengembangan Perangkat Lunak dan Gim</h3>
+          <p>Program keahlian yang fokus pada pengembangan aplikasi, pemrograman,membuat dan mengembangkan gim (game
+            development)..</p>
+        </div>
+      </div>
+
+      <div class="col-md-4">
+        <div class="jurusan-card">
+          <img src="assets/img/dkv.jpeg" alt="DKV" />
+          <h3>Desain Komunikasi Visual</h3>
+          <p>Program keahlian yang mempelajari tentang desain grafis, multimedia, dan komunikasi visual.</p>
+        </div>
+      </div>
+      <div class="col-md-4 mb-5">
+        <div class="jurusan-card">
+          <img src="assets/img/tbsm.jpeg" alt="TSM" />
+          <h3>Teknik Sepeda Motor</h3>
+          <p>Program keahlian yang mempelajari tentang otomotif motor</p>
+        </div>
+      </div>
+      <div class="col-md-4 mb-5">
+        <div class="jurusan-card">
+          <img src="assets/img/toi.jpeg" alt="TOI" />
+          <h3>Teknik Otomasi Industri</h3>
+          <p>Program keahlian yang mempelajari tentang elektronika</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  margin-top: 10%;
+}
+
+h1 {
+  color: #333;
+  font-size: 2rem;
+}
+
+.desc-text {
+  color: #666;
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+.jurusan-card {
+  background: #fff;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  padding: 15px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  min-height: 320px;
+  margin-top: 5%;
+}
+
+.jurusan-card img {
+  width: 120px;
+  /* Lebar gambar 120px */
+  height: 120px;
+  /* Tinggi gambar 120px */
+  object-fit: cover;
+  border-radius: 50%;
+  /* Membuat gambar berbentuk bulat */
+  border: 3px solid #ddd;
+  /* Menambahkan border tipis */
+  margin-bottom: 15px;
+}
+
+.jurusan-card h3 {
+  color: #444;
+  font-size: 1.2rem;
+  margin-bottom: 10px;
+}
+
+.jurusan-card p {
+  color: #666;
+  font-size: 0.9rem;
+  line-height: 1.5;
+  text-align: justify;
+}
+
+/* Grid untuk card */
+.row {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  /* 3 kolom di layar besar */
+  gap: 20px;
+}
+
+/* 2 kolom di layar sedang */
+@media (max-width: 992px) {
+  .row {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  .jurusan-card img {
+    width: 100px;
+    height: 100px;
+  }
+}
+
+/* 1 kolom di layar kecil */
+@media (max-width: 576px) {
+  .row {
+    grid-template-columns: 1fr;
+  }
+
+  .container {
+    padding: 10px;
+  }
+
+  h1 {
+    font-size: 1.8rem;
+  }
+
+  .jurusan-card img {
+    width: 80px;
+    height: 80px;
+  }
+}
+</style>
